945dbb0a47e00889c3f54f9615b4bf827bae22f0
asdf11048 <953866660@qq.com>
2025-11-30 21:24:08 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,4 +14,4 @@ diff ../m28c.config .config
 echo "make download"
 make download -j8 || { echo "download failed"; exit 1; }
 echo "make lede"
-make -j1 V=s || { echo "make failed"; exit 1; }
+make -j8 V=s || { echo "make failed"; exit 1; }
